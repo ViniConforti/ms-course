13cb977bb27d720382820ab739386fbdbc6e8371
Vinicius Conforti <confortivinicius@gmail.com>
2022-01-31 12:38:07 -0300

    Atualizando versao do spring boot em todos os projetos, para ser compativel com o Eureka; Adicionando propriedade spring.defer-datasource-initialization no projeto hr-worker, necessária para inicializar o banco com dados utilizando o eureka; Configurando Eureka clients.

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
@@ -2,10 +2,13 @@ package com.devsuperior.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name="hr-worker")
+@EnableEurekaClient
+
+//Eureka ja tem balanceamento de carga implementado
+//@RibbonClient(name="hr-worker")
 @SpringBootApplication
 @EnableFeignClients
 public class HrPayrollApplication {
