ea643b0cd8ab9ef8f5dfe541a0fdf95037fa1b19
Vinicius Conforti <confortivinicius@gmail.com>
2022-02-03 12:05:22 -0300

    Adicionada carga inicial de dados para o projeto hr-user

--- hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
@@ -1,15 +1,28 @@
 package com.devsuperior.hruser;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class HrUserApplication {
+/*@RequiredArgsConstructor
+@Log4j2*/
+public class HrUserApplication implements CommandLineRunner {
+
+	//private final BCryptPasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		/*String encryptedPass = "BCRYPT = " + passwordEncoder.encode("teste1@");
+		log.info(encryptedPass);*/
+	}
 }
